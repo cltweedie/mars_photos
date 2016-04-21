9c43d7373d7178f37dc40234a6c761f74ec3bbec
pcupueran <pa1809ty@gmail.com>
2016-04-21 13:50:30 +0100

    Refactor MarsPhotos to be a module

--- lib/mars_photos.rb
+++ lib/mars_photos.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 
-class MarsPhotos
+module MarsPhotos
   def get(rover:, sol:, cam:)
     HTTParty.get("https://api.nasa.gov/mars-photos/api/v1/rovers/#{rover}/photos?sol=#{sol}&camera=#{cam}&api_key=DEMO_KEY")
   end
