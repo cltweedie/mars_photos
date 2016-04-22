a2ec49569887e03f8d24823f97e97faebd3a3db6
pcupueran <pa1809ty@gmail.com>
2016-04-22 09:22:53 +0100

    Correct indentation and refactor get method

--- lib/mars_photos/rover.rb
+++ lib/mars_photos/rover.rb
@@ -1,14 +1,21 @@
 module MarsPhotos
-	class Rover
-		attr_accessor :name
+  class Rover
+    attr_accessor :name
+    def initialize(name)
+      @name = name
+    end
 
-		def initialize(name)
-			self.name = name
-		end
-
-		def get(sol)
-			response = HTTParty.get("https://api.nasa.gov/mars-photos/api/v1/rovers/#{self.name}/photos?sol=#{sol}&api_key=DEMO_KEY")
-      response['photos']
-		end
-	end
+    def get(parameter = {})
+      url_rovers = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
+      if parameter.keys.include? :earth_date
+        url = "#{url_rovers}#{self.name}/photos?earth_date=#{parameter[:earth_date]}&api_key=DEMO_KEY"
+        response = HTTParty.get(url)
+        response['photos']
+      else
+        url = "#{url_rovers}#{self.name}/photos?sol=#{parameter[:sol]}&api_key=DEMO_KEY"
+        response = TTParty.get(url)
+        response['photos']
+      end
+    end
+  end
 end
