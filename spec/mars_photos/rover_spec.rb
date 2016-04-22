5f664508c1696df0213a906de02e1fcaa7ca0e8d
Chris Tweedie <cltweedie@gmail.com>
2016-04-22 11:56:14 +0100

    Record HTTP requests with VCR

--- spec/mars_photos/rover_spec.rb
+++ spec/mars_photos/rover_spec.rb
@@ -8,19 +8,23 @@ describe MarsPhotos::Rover do
 
   describe '#get' do
     it "retrieves the photos in an array for a rover when given the sol" do
-      sol = 1000
-      response = rover.get(sol: sol)
-      expect(response.class).to eq(Array)
-      expect(response.first.keys.include?("img_src")).to be_truthy
-      expect(response.first.keys.include?("camera")).to be_truthy
+      VCR.use_cassette 'rover/get_sol' do
+        sol = 1000
+        response = rover.get(sol: sol)
+        expect(response.class).to eq(Array)
+        expect(response.first.keys.include?("img_src")).to be_truthy
+        expect(response.first.keys.include?("camera")).to be_truthy
+      end
     end
 
     it "retrieves photos in array for a rover when given the earth date" do
-      earth_date = "2015-6-3"
-      response = rover.get(earth_date: earth_date)
-      expect(response.class).to eq(Array)
-      expect(response.first.keys.include?("img_src")).to be_truthy
-      expect(response.first.keys.include?("camera")).to be_truthy
+      VCR.use_cassette 'rover/get_earth_date' do
+        earth_date = "2015-6-3"
+        response = rover.get(earth_date: earth_date)
+        expect(response.class).to eq(Array)
+        expect(response.first.keys.include?("img_src")).to be_truthy
+        expect(response.first.keys.include?("camera")).to be_truthy
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
 SimpleCov.start
 require 'mars_photos'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr'
+  c.hook_into :webmock
+end
